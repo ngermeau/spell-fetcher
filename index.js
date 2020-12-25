649f7aa3e9a92b9be8b09d4925673cdc26df540c
Nicolas Germeau <nicolas.germeau@gmail.com>
2020-12-25 20:13:37 +0100

    taking care of charset 8859 and refactoring

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ import cheerio from 'cheerio';
 import child from 'child_process';
 import csvw from 'csv-writer';
 
-const remoteServ = 'http://localhost:8080/'
-const filename = 'spells.csv'
-
 const spellProperties = [ { id: 'niveau', title: 'niveau' },
                           { id: 'nom', title: 'nom' }, 
                           { id: 'lien', title: 'lien' },
@@ -18,41 +15,80 @@ const spellProperties = [ { id: 'niveau', title: 'niveau' },
                           { id : 'Jet de sauvegarde', title: 'Jet de sauvegarde'}
                         ]
 
+const remoteServ = 'http://localhost:8080/'
+const download = false
+const spellsListUrl = "liste-classe-ensorceleur.htm"
+const filename = "ensorceleur.csv" 
+const createCsvWriter = csvw.createObjectCsvWriter
+const csvWriter = createCsvWriter({
+    path: filename,
+    header: spellProperties
+});
 
-// To download the assets avoiding getting ban from usage 
-async function downloadAssets(){
-  const listOfSpell = await getListOfSpells()
+/** 
+ * To download the assets, usefull for testing without getting banned 
+ * @param {string} listOfSpellsUrl  url containing the list of spells
+ */
+async function downloadAssets(listOfSpellsUrl){
+  child.exec('wget http://www.gemmaline.com/sorts/' + listOfSpellsUrl + ' -P assets') 
+  const listOfSpell = await getListOfSpells(listOfSpellsUrl)
   for (const spell of listOfSpell){
     await delay(3000)
     child.exec('wget http://www.gemmaline.com/sorts/' + spell['lien'] + ' -P assets') 
   }
 }
 
+/** 
+ * To delay each call avoiding getting banned 
+ * @param   {number}  ms  number of delay in millisecond
+ * @return  {Promise}     a promise resolved when the time is passed 
+ */
 function delay(ms){
   return new Promise((res) => setTimeout(res,ms))
 }
 
+/** 
+ * To cleanup string retrieved for each spell 
+ * @param   {string}  stringData  the string needed to be cleanup
+ * @return  {string}              cleaned up string
+ */
 function cleanup(stringData){
   return stringData.replace(':','').trim() 
 }
 
+/** 
+ * To retrieve an HTML page in the correct encoding 
+ * @param   {url}     url     url which needs to be retrieved 
+ * @return  {object}          cheerio object containing the url content 
+ */
+async function retrieveHtml(url){
+  const page = await fetch(remoteServ + url)
+  const buffer = await page.arrayBuffer() 
+  const isoHtml = new TextDecoder("iso-8859-1").decode(buffer)
+  return cheerio.load(isoHtml)
+
+}
+
+/** 
+ * To retrieve the detail of a spell 
+ * @param   {object} spell  contains name/url/level of the spell 
+ * @return  {object}        contains additionnal information
+ */
 async function getSpellDetail(spell){
-  const url = remoteServ + spell['lien'] 
-  const page = await fetch(url)
-  const text = await page.text() 
-  const html = cheerio.load(text)
+  const html = await retrieveHtml(spell['lien'])
   const ids = spellProperties.map((el) => { return el.id })
   html("strong").filter((i,el) => ids.includes(html(el).text()))
                        .map((i,el) =>  spell[html(el).text()] = cleanup(html(el)[0].next.data))
   return spell
 }
 
-async function getListOfSpells(){
-  const url = remoteServ + 'liste-classe-base-druide.htm'
-  const page = await fetch(url)
-  const text = await page.text()
-  const html  = cheerio.load(text) 
- 
+/** 
+ * To retrieve the list of spells 
+ * @param   {string}  listOfSpellsUrl   url containing the list of spells  
+ * @return  {array}                     the list of spells
+ */
+async function getListOfSpells(listOfSpellsUrl){
+  const html = await retrieveHtml(spellsListUrl)
   const listOfSpells = html("ul").map((currentLevel,spells) => {
                         return html(spells).find('a').map((index,spell) => {
                          return {
@@ -63,26 +99,28 @@ async function getListOfSpells(){
                         }).get()
                       }).get()
   return listOfSpells
-
 }
 
-async function process(){
-  const listOfSpells = await getListOfSpells()
-  const listOfDetailedSpells =  listOfSpells.map(spell => getSpellDetail(spell));
+/** 
+ * Entry point
+ * @param   {string}  listOfSpellsUrl   url containing the list of spells  
+ * @return  {array}                     of promise containing spell detail objects 
+ */
+async function process(listOfSpellsUrl){
+  const listOfSpells = await getListOfSpells(listOfSpellsUrl)
+  const listOfDetailedSpells = listOfSpells.map(spell => getSpellDetail(spell));
   return await Promise.all(listOfDetailedSpells)
 }
 
 
-const createCsvWriter = csvw.createObjectCsvWriter
-const csvWriter = createCsvWriter({
-    path: filename,
-    header: spellProperties
-});
-
-process().then((spells) => {
- csvWriter.writeRecords(spells)       
-   .then(() => {
-        console.log('...Done');
-    })
-})
+if (download == true) {
+  downloadAssets(spellsListUrl)
+} else {
+  process(spellsListUrl).then((spells) => {
+   csvWriter.writeRecords(spells)       
+     .then(() => {
+          console.log('...Done');
+      })
+  })
+}
 
